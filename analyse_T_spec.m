554c26f81d521233649e84de9ccdc0ef10583a0c
haelium <davidbourke94@gmail.com>
2016-04-12 00:25:54 +0100

    Added MEX file to generate normal distribution

--- analyse_T_spec.m
+++ analyse_T_spec.m
@@ -1,7 +1,7 @@
-J = normrnd(-3, 1, 20);
+J = normrnd(-3, 0, 20);
 T = 1;
 protein_length = 27;
-number_of_runs = 5*10^7;
+number_of_runs = 5*10^6;
 init_protein = generate_protein(protein_length);
 [E, S, L, MCS_T, min_protein, final_protein] = fold_protein(init_protein, T, number_of_runs, J);
 filename = strcat('S-', mat2str(protein_size(min_protein,J)), '_after_', mat2str(MCS_T), '_steps.mat');
